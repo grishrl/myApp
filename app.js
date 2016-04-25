3239d1f81e0510b64ad4ac9282b37e02dde74113
grishrl <lee_grisham05@yahoo.com>
2016-04-25 00:03:47 -0500

    add post methods, credit/debit, form validation

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 
+var bodyParser = require("body-parser");
+var jsonParse = bodyParser.json();
+
+var underScore = require("underscore");
+
 app.use('/',express.static('public'));
 
 app.get('/',function(req,res){
@@ -9,16 +14,22 @@ app.get('/',function(req,res){
 
 var someData=[
 	{
-		"date":"10/10/10",
-		"amount":50
+		"date":"2016-04-25T03:21:58.756Z",
+		"amount":50,
+		"total":50,
+		"type":"credit"
 	},
 	{
-		"date":"10/10/10",
-		"amount":100
+		"date":"2016-04-25T03:22:04.362Z",
+		"amount":100,
+		"total":150,
+		"type":"credit"
 	},
 	{
-		"date":"10/10/10",
-		"amount":25
+		"date":"2016-04-25T03:24:04.748Z",
+		"amount":25,
+		"total":175,
+		"type":"credit"
 	}
 	];
 	
@@ -26,8 +37,31 @@ app.get('/transacts',function(req,res){
 	res.json(someData);
 });
 
-app.put('/pay',function(req,res){
-	someData.push(req.data);
+app.post('/pay',jsonParse,function(req,res){
+	console.log(req.body);
+	var tab = someData.length-1;
+	if(req.body.amount!==undefined){
+			var curTot = Number(someData[tab].total);
+		if(req.body.type==='debit'){
+			 
+			 curTot -= Number(req.body.amount);
+		}else{
+			 console.log(curTot + " before changes");
+			 curTot += Number(req.body.amount);
+			 console.log(curTot + "after math");
+		}
+		var manipulateData={
+			'date':req.body.date,
+			'amount':req.body.amount,
+			'total':curTot,
+			'type':req.body.type
+		};
+		console.log(manipulateData);
+		someData.push(manipulateData);
+		res.sendStatus(201);
+	}
+	
+	
 });
 
 app.listen(8080, function(){
--- public/client/credit.js
+++ public/client/credit.js
@@ -1,4 +1,15 @@
 angular.module("creditcard",['ngRoute'])
+.factory('TransX', ['$http',function TransXFactory($http){
+	return {
+	all: function(){
+		return $http({method:'GET',url:'/transacts'});
+		},
+	submit: function(xdat){	
+	return $http({method:'POST',url:'/pay',data:xdat});
+	}	
+		
+	};
+}])
 .config(function($routeProvider){
 	$routeProvider
 	.when('/',{
@@ -12,8 +23,42 @@ angular.module("creditcard",['ngRoute'])
 	})
 	.otherwise({redirectTo:'/transactions'})
 	})
-.controller('transactionController',['$http',function($http){
-	$http({method:'GET',url:'/transacts'}).then((res)=>{
-		this.message=res.data;
+.controller('transactionController',['TransX',function(TransX){
+	TransX.all().then((res)=>{
+		var temp = res.data;
+		var temp = _.sortBy(temp,'date').reverse();
+		this.message=temp;
+		
 	},function(){console.log("no data returned")});
+}])
+.controller('submitController',['$scope','$location','TransX',function($scope,$location,TransX){
+	var type;
+	if($location.path()=='/payment'){
+		type='debit';
+	}else{
+		type='credit';
+	}
+	
+ $scope.save=function(inDat){
+	var date=new Date();
+	console.log(date);
+    var amount=inDat;
+	var transAct = {"date":date,
+					"amount":amount,
+					"type":type};
+	TransX.submit(transAct).then((res)=>{
+		if(res.status==201){
+			if($location.path()!='/transactions'){$location.path('/transactions');}
+			window.location.reload();
+		}
+	},()=>alert("Error posting."));
+};
+}])
+.controller('totalController',['TransX',function(TransX){
+	TransX.all().then((res)=>{
+		var temp = res.data;
+		var temp = _.sortBy(temp,'date').reverse();
+		var temp = temp[0].total;
+		this.message=temp;
+		},function(){console.log("no data returned")});
 }]);
